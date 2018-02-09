70c355eefa4258fcf95b2b0be1451a67ae11cd91
mozell <mozellatto@gmail.com>
2018-02-09 20:55:26 +0900

    #05

---
+++ JumptoPython/02.7.py
@@ -0,0 +1,15 @@
+#자료형의 참과 거짓
+a = [1, 2, 3, 4]
+while a :           # a 가 참일 동안
+    print(a.pop())
+
+if []:              # [] 가 참이면 True 거짓이면 False 출력
+    print("True")
+else:
+    print("False")
+
+
+if [1,2,3]:         # [1,2,3] 참이면 True 거짓이면 False 출력
+    print("True")
+else:
+    print("False")
